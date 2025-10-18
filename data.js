7b26c1fe397e52abd8d79338ec21bb92aab577f7
baliberbagiii-spec <bali.berbagiii@gmail.com>
2025-10-19 04:21:33 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,4 @@
+const projects=[
+{id:'yeh-gangga',title:'Yeh Gangga',location:'Tabanan, Bali',image:'https://images.unsplash.com/photo-1493558103817-58b2924bce98?w=600',price:'Rp 530.000.000 / 100m²',description:'Tanah kavling dekat Pantai Yeh Gangga.'},
+{id:'jl-muding',title:'Jl. Muding Mekar',location:'Kerobokan Kaja, Bali',image:'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=600',price:'Rp 900.000.000 / are',description:'Lokasi strategis dekat pusat aktivitas.'}
+];
